764a0e5429df28b03435a0f20f09044ccbf89737
phamtanphat <phatdroid94@gmail.com>
2023-06-27 18:22:30 +0700

    Initial commit

---
+++ lib/presentation/page/search_temp_city_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class SearchTempCityPage extends StatefulWidget {
+  const SearchTempCityPage({Key? key}) : super(key: key);
+
+  @override
+  State<SearchTempCityPage> createState() => _SearchTempCityPageState();
+}
+
+class _SearchTempCityPageState extends State<SearchTempCityPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
