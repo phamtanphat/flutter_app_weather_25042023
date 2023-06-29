a61932151123ab6106af38c0ab35bd7dc959b42b
Phat <phatdroid94@gmail.com>
2023-06-29 18:37:54 +0700

    tach code

---
+++ lib/common/app_constants.dart
@@ -0,0 +1,3 @@
+class AppConstants {
+  static const String BASE_URL = "https://api.openweathermap.org/";
+}
--- lib/data/api/dio_client.dart
+++ lib/data/api/dio_client.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_app_weather_25042023/common/app_constants.dart';
 
 class DioClient {
   // Singleton pattern
@@ -10,7 +11,7 @@ class DioClient {
       Dio dio = Dio(BaseOptions(
         connectTimeout: const Duration(seconds: 30),
         receiveTimeout: const Duration(seconds: 30),
-        baseUrl: "https://api.openweathermap.org/"
+        baseUrl: AppConstants.BASE_URL
       ));
 
       dio.interceptors.add(LogInterceptor());
